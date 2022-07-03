2aeab28c62f283c6ff4bd7f607d880bfe8319bce
thomsart <psychoid@hotmail.fr>
2022-07-03 18:04:53 +0200

    add models ProducttCategory, ProductNale, Product

--- api/sales/models.py
+++ api/sales/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+
+class ProductCategory(models.Model):
+    category = models.CharField(max_length=30, blank=False)
+    class Meta:
+        ordering = ['category']
+
+
+class ProductName(models.Model):
+    name = models.CharField(max_length=30, blank=False)
+    class Meta:
+        ordering = ['name']
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.ForeignKey(ProductName, on_delete=models.CASCADE)
+    manufacturing_cost = models.DecimalField()
+    price = models.DecimalField()
+    created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['category', 'name']
---
+++ api/sales/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from models import *
+
+
+
+class ProductCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductCategory
+        fields = '__all__'
+
+
+class ProductNameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductName
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
